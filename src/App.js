a138e68f425efd43000fdbea07e401b29d839f66
Brendan Bard <bbard@purdue.edu>
2017-07-24 09:53:49 -0400

    added input and button WIP handle submit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Route, Switch, NavLink} from 'react-router-dom' 
-import base, {auth} from './base'
+import base, {auth} from './base.js'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state ={
+      name: '',
+      id: null
+    }
+  }
+  handleSubmit = (ev) =>{
+   ev.preventDefault()
+   this.inputState()
+   console.log(this.state)
+  }
+  inputState = () => {
+    const f = document.getElementById('input').value
+    this.name = f
+    this.id = Date.now()
+  }
+  
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Let's party</h2>
-          <ul className='nav-links'>
+        </div>
+        <form className="user-input">
+        <input type='text' id='input' className='name'/>
+        <button className='submit-button' type='submit' onClick={this.handleSubmit}>Submit</button>
+        </form>
+        <ul className='nav-links'>
             <li>
-              <NavLink to='/nope'/>
+              
               </li>
               </ul>
-        </div>
-        
       </div>
     );
   }
